8da7b2f09b2486adc6653b1fcdfcfb37adaf774f
ianwinson <iankwinson@gmail.com>
2018-02-25 14:23:10 -0600

    Untested Elevator Encoder Correction Logic

--- src/org/usfirst/frc/team2848/robot/commands/elevator/GoToHeight.java
+++ src/org/usfirst/frc/team2848/robot/commands/elevator/GoToHeight.java
@@ -10,19 +10,24 @@ import edu.wpi.first.wpilibj.command.Command;
  *
  */
 public class GoToHeight extends Command {
-
-	double target;
+	double lastTick = 0;
+	double target = 0;
 	Timer t = new Timer();
 	public GoToHeight(double x) {// use target height as parameter
 		requires(Robot.elevator);
+		
 		target = x;
 	}
 
 	protected void initialize() {
+		lastTick = Robot.elevator.elevatorEncoder.get();
 		t.start();
 	}
 
 	protected void execute() {
+		if(t.get() ==.1 && Robot.elevator.elevatorEncoder.get()<lastTick +5) {
+			end();
+		}
 		if(t.get()>.2)
 			Robot.pivotIntake.intakePivot.set(DoubleSolenoid.Value.kForward);
 		Robot.elevator.goToPosition(target);
